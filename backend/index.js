c3b46abc027e498b55fb0e9452b30792b5c93b39
leon-hub101 <L.Pretorius07@gmail.com>
2025-01-08 10:32:01 +0200

    Configured MongoDB and Mongoose for the back-end

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+const mongoURI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/credsecure";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
